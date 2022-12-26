6352f32104f7968b62cc56c5886c4382999cd33b
asd <west849@naver.com>
2022-12-26 18:18:31 +0900

    com

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,20 @@
 $(function(){
+
+    //팝업열기 닫기기능
+    /**
+     * @popFrame 아이디값을 넣으세요
+     *  */
+    function popClose(popFrame){
+        $(popFrame).css('display','none');
+    }
+
+    function popOpen(popFrame){
+        $(popFrame).css('display','block');
+    }
+
+
+
+
     /**
      * header 
      */
@@ -72,6 +88,20 @@ $(function(){
      })
 
 
+
+     //전체 약관
+     
+
+
+
+
+
+
+
+
+
+
+
     //  #monthly
 
     $('#monthly .info-area2 .arrow').click(function(e){
@@ -120,6 +150,136 @@ $(function(){
     $('.radio-chk').click(function (e) { 
         $(this).addClass('active').siblings('.radio-chk').removeClass('active');
     });
+
+
+
+    // 4-0신청내역조회
+    // appli1
+    $('#appli1 .radio-box label').click(function(){
+
+        if($(this).siblings('input').val() == 'phone'){
+            $('#phone').css('display','flex')
+            $('#mail').css('display','none')
+        }else{
+            $('#mail').css('display','flex')
+            $('#phone').css('display','none')
+        }
+    })
+
+
+    $('#pay2 .identity label').click(function(){
+
+        target = $(this).siblings('input').val();
+        $('#'+target).addClass('active').siblings().removeClass('active')
+    })
+
+
+    $('#exservice2 .addservice label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#'+target).addClass('active').siblings().removeClass('active')
+    })
+
+
+    $('#joinFrom1 #sort label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinFrom1 #'+target).css('display','block').siblings().css('display','none')
+    })
+
+    $('#joinFrom1 #isSim label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinFrom1 #'+target).css('display','block').siblings().css('display','none')
+
+        if(target == 'has1'){
+            $('#joinFrom1 #has1-1').css('display','block').siblings().css('display','none')
+        }else{
+            $('#joinFrom1 #has1-2').css('display','block').siblings().css('display','none')
+
+        }
+       
+    })
+
+
+
+
+    $('#joinFrom1 #has1-1 label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#'+target).css('display','block').siblings().css('display','none')
+    })
+    $('#joinFrom1 #has1-2 label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinFrom1 #'+target).css('display','block').siblings().css('display','none')
+    })
+
+
+
+    $('#joinFrom1 .fix-total .btn.top').click(function(){
+        $('html,body').animate({scrollTop:0},300);
+        return false;
+    })
+
+
+
+    // 17-1 온라인_가입신청서2
+    $('#joinFormWrap .group-nav2 a').click(function(e){
+        e.preventDefault();
+        target = $(this).data('target');
+        $(this).addClass('active').siblings().removeClass('active')
+        $(target).addClass('on').siblings().removeClass('on')
+    })
+    // 실명 확인 증빙 자료
+    $('#joinForm01 .id-radio1 label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinForm01 #'+target).css('display','block').siblings().css('display','none')
+    })
+    $('#joinForm2 .id-radio2 label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinForm2 #'+target).css('display','block').siblings().css('display','none')
+    })
+    $('#joinForm2 .id-radio3 label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinForm2 #'+target).css('display','block').siblings().css('display','none')
+    })
+
+    //본인인증 방법
+    $('#joinForm01 .auth1-chk label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinForm01 #'+target).css('display','block').siblings().css('display','none')
+    })
+
+
+    $('#joinForm2 .auth2-chk label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinForm2 #'+target).css('display','block').siblings().css('display','none')
+    })
+    $('#joinForm2 .auth3-chk label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinForm2 #'+target).css('display','block').siblings().css('display','none')
+    })
+    $('#joinForm2 .auth4-chk label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinForm2 #'+target).css('display','block').siblings().css('display','none')
+    })
+
+    // 납부 수단별 노출
+    $('#joinFormData .method-area label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinFormData #'+target).css('display','block').siblings().css('display','none')
+    })
+
+
+    // selfform1
+    // 셀프 개통가입신청서
+    $('#joinForm2 .id-radio4 label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinForm2 #'+target).css('display','block').siblings().css('display','none')
+    })
+   
+
+    $('#joinForm2 .method-area label').click(function(){
+        target = $(this).siblings('input').val();
+        $('#joinForm2 #'+target).css('display','block').siblings().css('display','none')
+    })
+
 
 
 })
